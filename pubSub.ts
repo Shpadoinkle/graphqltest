f4fcf86a4e2dd00fc2b6dff1f92b92dda5ca19ee
Ash <ash@Ashs-MBP.modem>
2019-11-19 00:07:58 +1000

    configs

--- pubSub.ts
+++ pubSub.ts
@@ -1,13 +1,13 @@
-import {PostgresPubSub} from 'graphql-postgres-subscriptions'
+import { PostgresPubSub } from "graphql-postgres-subscriptions";
 
 export const PubSub = new PostgresPubSub({
-  user: process.env.DATABASE_USERNAME,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: 5432,
-})
+  user: "postgres",
+  host: "test1.cprw2mvph38r.ap-southeast-2.rds.amazonaws.com",
+  database: "table1",
+  password: "gweggweg",
+  port: 5432
+});
 
-PubSub.subscribe('error', err => {
-  console.log('@@@ PubSub Error:', err)
-})
+PubSub.subscribe("error", err => {
+  console.log("@@@ PubSub Error:", err);
+});
