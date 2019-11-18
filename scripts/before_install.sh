379f1219611667d4da1e3659ce43fafb00dd590f
Ash <ash@Ashs-MBP.modem>
2019-11-18 22:54:16 +1000

    remove stuf

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+# Install node.jsremove 
 
 # Install nodemon
 # sudo npm install nodemon -g
